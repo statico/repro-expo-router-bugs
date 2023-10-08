66a12cd5ca5f41f14ff42e285381d50a42000f88
Ian Langworth <ian@langworth.com>
2023-10-08 15:37:56 -0700

    Initial repro

---
+++ app/_layout.tsx
@@ -0,0 +1,13 @@
+import { Stack } from "expo-router";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+export default function Layout() {
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaProvider>
+        <Stack />
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
+  );
+}
---
+++ index.js
@@ -0,0 +1,3 @@
+// We have to use an index.js instead of a "main" key in package.json because
+// of yet another bug: https://github.com/expo/router/issues/748
+import "expo-router/entry";

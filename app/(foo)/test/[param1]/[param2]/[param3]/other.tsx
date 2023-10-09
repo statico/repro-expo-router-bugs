665dcc5779196067c0a7ecd98b250fe99bd709c2
Ian Langworth <ian@langworth.com>
2023-10-08 17:44:10 -0700

    Add relative link

--- app/(foo)/test/[param1]/[param2]/[param3]/other.tsx
+++ app/(foo)/test/[param1]/[param2]/[param3]/other.tsx
@@ -5,9 +5,10 @@ export default function Page() {
   const params = useLocalSearchParams();
   return (
     <View style={{ flex: 1, padding: 20 }}>
+      <Text>This is the other page.</Text>
       <Text>Got params: {JSON.stringify(params)}</Text>
-      <Link href="./other" asChild>
-        <Button title="Go to other page (relative)" />
+      <Link href="./details" asChild>
+        <Button title="Go back to details (relative)" />
       </Link>
       <Link href="/" asChild>
         <Button title="Go home" />

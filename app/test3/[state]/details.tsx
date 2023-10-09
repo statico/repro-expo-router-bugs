15bd0254d564bbd6757806986c34af163cd89dd8
Ian Langworth <ian@langworth.com>
2023-10-08 19:13:13 -0700

    WOrks without a monorepo

--- app/test3/[state]/details.tsx
+++ app/test3/[state]/details.tsx
@@ -5,11 +5,8 @@ export default function Page() {
   const params = useLocalSearchParams();
   return (
     <View style={{ flex: 1, padding: 20 }}>
-      <Text>This is the other page.</Text>
+      <Text style={{ fontSize: 20 }}>Test 3 Details</Text>
       <Text>Got params: {JSON.stringify(params)}</Text>
-      <Link href="./details" asChild>
-        <Button title="Go back to details (relative)" />
-      </Link>
       <Link href="/" asChild>
         <Button title="Go home" />
       </Link>

5714f93029d09e98ec3574cda53b4a5f1842abfc
Ian Langworth <ian@langworth.com>
2023-10-08 18:54:09 -0700

    Still works

--- packages/mobile/app/index.jsx
+++ packages/mobile/app/index.jsx
@@ -5,7 +5,7 @@ export default function Page() {
   return (
     <View style={{ flex: 1, padding: 20 }}>
       <Text>This is the index page with no group or layout.</Text>
-      <Link href="/courts/aaaaa/bbbbb/ccccc/ddddd/details" asChild>
+      <Link href="/test/aaaaa/bbbbb/ccccc/ddddd/details" asChild>
         <Button title="Test 1" />
       </Link>
       <Link href="/test/new" asChild>
